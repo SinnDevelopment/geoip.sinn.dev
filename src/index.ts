cd67d2618d7a036192a028e84585b472a1462fa4
Jamie Sinn <james.sinn@sinndevelopment.com>
2025-01-02 19:52:55 -0500

    Setup initial geofeeds

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,14 @@
-import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 
 // Start a Hono app
 const app = new Hono();
 
-// Setup OpenAPI registry
-const openapi = fromHono(app, {
-	docs_url: "/",
-});
+const geoFeeds =
+	'23.182.72.0/25,CA,CA-ON,Stratford,\n' +
+	//'23.182.72.128/25,US'
+	'2602:F8A1:1::/48,CA,CA-ON,Stratford,'
 
-// Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
-
+app.get('/geofeed.csv', (c) => c.text(geoFeeds))
+app.get('/', (c) => c.text(geoFeeds))
 // Export the Hono app
 export default app;
